d261895e5993e5ed8460158a8020abd197429166
jordi <jordi.lanau@yahoo.com>
2022-03-23 15:34:15 +0000

    Add controllers to get a User by ID, update a User by ID and delete a User by ID,

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 
-function getUsers(req, res) {
+function getUsers(_, res) {
   User.find()
     .then((users) => res.status(200).json(users))
     .catch((error) => res.status(500).json(error));
@@ -12,4 +12,40 @@ function createUser(req, res) {
     .catch((error) => res.status(500).json(error));
 }
 
-module.exports = { getUsers, createUser };
+function getUserByID(req, res) {
+  User.findOne({ _id: req.params.userID })
+    .select('-__v')
+    // .populate('thoughts')
+    // .populate('friends')
+    .then((user) =>
+      user
+        ? res.status(200).json(user)
+        : res.status(404).json({ message: 'No user with that ID' })
+    )
+    .catch((err) => res.status(500).json(err));
+}
+
+function updateUser(req, res) {
+  User.findOneAndUpdate(
+    { _id: req.body.id },
+    { $set: { username: req.body.username, email: req.body.email } }
+  )
+    .then((user) =>
+      user
+        ? res.status(200).json(user)
+        : res.status(404).json({ message: 'No user with that ID' })
+    )
+    .catch((err) => res.status(500).json(err));
+}
+
+function deleteUser(req, res) {
+  User.findOneAndDelete({ _id: req.body.id })
+    .then((user) =>
+      user
+        ? res.status(200).json(user)
+        : res.status(404).json({ message: 'No user with that ID' })
+    )
+    .catch((err) => res.status(500).json(err));
+}
+
+module.exports = { getUsers, createUser, getUserByID, updateUser, deleteUser };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
-const { getUsers, createUser } = require('../../controllers/userController');
+const {
+  getUsers,
+  createUser,
+  getUserByID,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController');
 
 // /api/users
-router.route('/').get(getUsers).post(createUser);
+router
+  .route('/')
+  .get(getUsers)
+  .post(createUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+// /api/users/:userID
+router.route('/:userID').get(getUserByID);
 
 module.exports = router;
